de16192dc0d107dca828716e64baccc8e0d2e43a
theEmris <emirsmorganson2000@gmail.com>
2022-04-18 06:01:49 +0500

    bottom navigation bar added

---
+++ lib/core/components/pageNames.dart
@@ -0,0 +1,13 @@
+import 'package:my_app/screens/homePageView.dart';
+import 'package:my_app/screens/profilePageView.dart';
+import 'package:my_app/screens/savedPageView.dart';
+
+class PageNames {
+  static List forNavBar = [
+
+    HomePageView(),
+    SavedPageView(),
+    ProfilePageView(),
+
+  ];
+}
---
+++ lib/widgets/bottomNavBar.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/core/components/pageNames.dart';
+
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomNavBar> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BottomNavigationBar Sample'),
+      ),
+      body: Center(
+        child: PageNames.forNavBar.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark),
+            label: 'Saved',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
