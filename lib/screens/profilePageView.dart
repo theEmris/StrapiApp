39d2d304e85de153659c43073fe05cdd13c353cc
theEmris <emirsmorganson2000@gmail.com>
2022-04-18 05:52:03 +0500

    main pages are added and main files were added

---
+++ lib/screens/profilePageView.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ProfilePageView extends StatelessWidget {
+  const ProfilePageView({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("profile page"),
+      ),
+      
+    );
+  }
+}
